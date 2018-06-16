9ad769770961291d06fc00f89a83224d1cba506a
FrankSpencer590 <sammybobmiller1@gmail.com>
2018-06-16 22:03:41 +0100

    Initial Commit

---
+++ Cracksoft App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cracksoft App
+//
+//  Created by Samuel Miller on 16/06/2018.
+//  Copyright © 2018 Cracksoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
